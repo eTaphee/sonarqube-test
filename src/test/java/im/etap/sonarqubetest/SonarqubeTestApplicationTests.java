44671a03170dd65c8ee540c6b8e463f36ac15aba
thlee <e-tap@naver.com>
2024-10-14 15:32:05 +0900

    Update SonarqubeTestApplicationTests.java

--- src/test/java/im/etap/sonarqubetest/SonarqubeTestApplicationTests.java
+++ src/test/java/im/etap/sonarqubetest/SonarqubeTestApplicationTests.java
@@ -1,13 +1,21 @@
 package im.etap.sonarqubetest;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SonarqubeTestApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        assertNotNull(applicationContext);
     }
 
 }
