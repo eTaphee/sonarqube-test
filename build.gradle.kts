c0483979a32687029c0438c954fd5caea5358c92
thlee <e-tap@naver.com>
2024-10-14 14:55:45 +0900

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,17 @@ plugins {
     java
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
+    id("org.sonarqube") version "4.0.0.2929"
 }
 
 group = "im.etap"
 version = "0.0.1-SNAPSHOT"
+
+sonar {
+    properties {
+        property("sonar.projectName", "BIGS-CO-LTD_bigs-pay-backend-spring_AZKI_tW4Olz6suGrlpW3")
+    }
+}
 
 java {
     toolchain {
@@ -18,7 +25,6 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
