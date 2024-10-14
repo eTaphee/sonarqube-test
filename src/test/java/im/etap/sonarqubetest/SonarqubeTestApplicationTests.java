1687e8adcc0ede326df235de8d9d878a411746d0
thlee <e-tap@naver.com>
2024-10-14 14:01:11 +0900

    init

---
+++ src/test/java/im/etap/sonarqubetest/SonarqubeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package im.etap.sonarqubetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
